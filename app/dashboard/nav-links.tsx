148faa68eb66592dc12b050d89f7bbb5d739ef74
Jahirul Islam <86736791+jahirulislm@users.noreply.github.com>
2024-03-06 17:00:00 +0600

    nav links fixed

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -3,6 +3,23 @@ import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import {
+  UserGroupIcon,
+  HomeIcon,
+  DocumentDuplicateIcon,
+} from '@heroicons/react/24/outline';
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
+
 function NavLinks() {
   const pathname = usePathname();
   return (
