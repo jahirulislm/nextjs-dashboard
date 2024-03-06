1e459d708647da9be5b7e8c5808dbc4adf829a91
Jahirul Islam <86736791+jahirulislm@users.noreply.github.com>
2024-03-06 19:35:39 +0600

    change content

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,9 @@ export default function Page() {
           <p
             className={`${lusitana.className}text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome Here!</strong> T{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              Explore
             </a>
           </p>
           <Link
