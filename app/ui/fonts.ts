3b830d45a4f357f5c08b4876fc0601dfbae81921
Jahirul Islam <86736791+jahirulislm@users.noreply.github.com>
2024-03-06 16:05:21 +0600

    dashboard entry

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Roboto_Mono, Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const inter = Inter({ subsets: ['latin'] });
